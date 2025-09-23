edb0af67a8a9e645b84291f9acde2b93d582e994
Vladimir Vasilev <vasilev.vladimir@yahoo.com>
2025-09-23 14:16:31 +0300

    Update

--- SauceDemo.Automation/obj/Debug/net9.0/SauceDemo.Automation.AssemblyInfo.cs
+++ SauceDemo.Automation/obj/Debug/net9.0/SauceDemo.Automation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SauceDemo.Automation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bca12abcda9c9491594ae5217543c5ec9651c37")]
 [assembly: System.Reflection.AssemblyProductAttribute("SauceDemo.Automation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SauceDemo.Automation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
